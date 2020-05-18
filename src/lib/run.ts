b6058523756481c6a23547400448bbb57e580110
francois <fran@pop-os.localdomain>
2020-05-18 17:14:25 +0200

    v1.10.3

--- src/lib/run.ts
+++ src/lib/run.ts
@@ -1,5 +1,5 @@
 import { join, dirname } from 'path'
-import { spawn } from 'child_process'
+import { spawn, exec } from 'child_process'
 import clipop from 'clipop'
 import colors from 'colors'
 import { compact, isEmpty, some } from 'lodash'
@@ -13,6 +13,7 @@ import getJson from '../helpers/getJson'
 import findContext from '../helpers/findContext'
 import parseArgs from '../helpers/parseArgs'
 import ensureRequired from '../helpers/ensureRequired'
+import applyTemplate from '../helpers/applyTemplate'
 
 export default async function run(app: string, ...args: string[]) {
   let ctx: any
@@ -44,92 +45,102 @@ export default async function run(app: string, ...args: string[]) {
     if (options.help) {
       return await help(ctx)
     }
-
-    if (!ctx.entry) {
-      if (!isEmpty(ctx.commands)) {
-        throw new Error('Missing command')
-      }
-      return ''
-    }
   
     const params = await parseArgs(ctx.options, options)
 
     ensureRequired(ctx.options, params)
 
-    const tmp = `/tmp/clipman-${ shortid.generate() }.json`
-
-    const clipmanOptions = {
-      arguments: args,
-    }
-
-    const execArgs = [
-      join(base, ctx.entry),
-      JSON.stringify(params),
-      JSON.stringify(clipmanOptions),
-      tmp
-    ]
-
-    return new Promise((resolve, reject) => {
-      const ps = spawn(
-        'node',
-        [join(__dirname, 'execCommand'), ...execArgs],
-        { cwd: base }
-      )
-      ps.stdout.on('data', data => {
-        console.log(data.toString())
-      })
-      ps.stderr.on('data', data => {
-        console.log(colors.italic.grey(data.toString()))
-      })
-      ps.on('close', async (status) => {
-        let response: Buffer | null = null
-        let res: any = null
-        try {
-          response = await promisify(readFile)(tmp)
-        } catch (error) {
-          if (status !== 0) {
-            reject(new Error(`Command failed with status ${ status } and response was not saved`))
-            return
-          }
+    if (ctx.command) {
+      try {
+        const { stdout } = await promisify(exec)(applyTemplate(ctx.command, params))
+        console.log(stdout)
+      } catch (error) {
+        console.log(error.stdout)
+        process.exit(1)
+      }
+    } else {
+      if (!ctx.entry) {
+        if (!isEmpty(ctx.commands)) {
+          throw new Error('Missing command')
         }
-        if (response) {
+        return ''
+      }
+      
+      const tmp = `/tmp/clipman-${ shortid.generate() }.json`
+
+      const clipmanOptions = {
+        arguments: args,
+      }
+
+      const execArgs = [
+        join(base, ctx.entry),
+        JSON.stringify(params),
+        JSON.stringify(clipmanOptions),
+        tmp
+      ]
+
+      return new Promise((resolve, reject) => {
+        const ps = spawn(
+          'node',
+          [join(__dirname, 'execCommand'), ...execArgs],
+          // { cwd: base }
+        )
+        ps.stdout.on('data', data => {
+          console.log(data.toString())
+        })
+        ps.stderr.on('data', data => {
+          console.log(colors.italic.grey(data.toString()))
+        })
+        ps.on('close', async (status) => {
+          let response: Buffer | null = null
+          let res: any = null
           try {
-            res = JSON.parse(response.toString())
+            response = await promisify(readFile)(tmp)
           } catch (error) {
-            if (status === 0) {
-              resolve('')
+            if (status !== 0) {
+              reject(new Error(`Command failed with status ${ status } and response was not saved`))
               return
             }
-            reject(new Error(`Command failed with status ${ status } and response was not parsed`))
-            return
           }
-        }
-        if (res) {
-          if (status === 0) {
-            if (res.error) {
-              let err = new Error(res.error.message)
-              for (const key in res.error) {
-                err[key] = res.error[key]
+          if (response) {
+            try {
+              res = JSON.parse(response.toString())
+            } catch (error) {
+              if (status === 0) {
+                resolve('')
+                return
               }
-              reject(err)
-            } else {
-              resolve(res.response)
-            }
-          } else {
-            if (res.error) {
-              let err = new Error(res.error.message)
-              for (const key in res.error) {
-                err[key] = res.error[key]
-              }
-              reject(err)
-            } else {
-              reject(new Error(`Command failed with status ${ status }`))
+              reject(new Error(`Command failed with status ${ status } and response was not parsed`))
+              return
             }
           }
-        }
+          if (res) {
+            if (status === 0) {
+              if (res.error) {
+                let err = new Error(res.error.message)
+                for (const key in res.error) {
+                  err[key] = res.error[key]
+                }
+                reject(err)
+              } else {
+                resolve(res.response)
+              }
+            } else {
+              if (res.error) {
+                let err = new Error(res.error.message)
+                for (const key in res.error) {
+                  err[key] = res.error[key]
+                }
+                reject(err)
+              } else {
+                reject(new Error(`Command failed with status ${ status }`))
+              }
+            }
+          }
+        })
+        ps.on('error', reject)
       })
-      ps.on('error', reject)
-    })
+    }
   } catch (error) {
     const regexes: (RegExp | string)[] = [
       /^Missing required option: /,
